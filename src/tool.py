1b261cbcaa082591a3a58841ec61ed18dce6e9a9
Yi-Heng Tsai <tsaiyiheng.tw@gmail.com>
2026-05-26 04:14:24 +0000

    update

--- src/tool.py
+++ src/tool.py
@@ -10,12 +10,13 @@ def get_metrics(y, y_pred, model_name, data):
       "data": data,
       "r^2": r2_score(y, y_pred),
       "mae": mean_absolute_error(y, y_pred),
-      "mse": mean_squared_error(y, y_pred)
+      "mse": mean_squared_error(y, y_pred),
+      "rmse": mean_squared_error(y, y_pred) ** 0.5
   }
 
 def plot_true_vs_pred(test, pred, name):
-  plt.figure(figsize=(6,6), alpha=0.3)
-  plt.scatter(test, pred)
+  plt.figure(figsize=(6,6))
+  plt.scatter(test, pred, alpha=0.3)
   plt.plot([min(test), max(test)], [min(test), max(test)], 'r--')  # y=x line
   plt.xlabel('True')
   plt.ylabel('Predicted')
