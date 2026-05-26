14d34c13617db7841b704cc5a16d6127fd313046
Yi-Heng Tsai <tsaiyiheng.tw@gmail.com>
2026-05-26 03:27:04 +0000

    Initial project commit

---
+++ src/tool.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from pathlib import Path
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+FIG_DIR = Path("/content/drive/MyDrive/California-housing/figures")
+
+def get_metrics(y, y_pred, model_name, data):
+  return {
+      "model": model_name,
+      "data": data,
+      "r^2": r2_score(y, y_pred),
+      "mae": mean_absolute_error(y, y_pred),
+      "mse": mean_squared_error(y, y_pred)
+  }
+
+def plot_true_vs_pred(test, pred, name):
+  plt.figure(figsize=(6,6), alpha=0.3)
+  plt.scatter(test, pred)
+  plt.plot([min(test), max(test)], [min(test), max(test)], 'r--')  # y=x line
+  plt.xlabel('True')
+  plt.ylabel('Predicted')
+  plt.title(f"True vs. Predicted {name}")
+  plt.savefig(f"{FIG_DIR}/True vs. Predicted {name}.png", dpi=300, bbox_inches="tight")
+  plt.show()
